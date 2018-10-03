a691253be4ab5d656dd8d853b8f48e03cc511a69
Fábio Cícero <devfabiocicero@gmail.com>
2018-10-03 17:59:58 -0300

    travis added to project

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ const API_URL = 'https://api.spotify.com/v1';
 const TOKEN_API = 'BQDGf_-r1fDb9f4I88etqi2pfoUsDuYivMBuSAHoI6WbuB21TKxtue8-3EnUWYK_M93tN18NExKOJomPJ5hrH_-6luPg9AmWb9pRTE_AK1Lvth-GPOt-mAHQUO7S7x5DpAMpm04';
 
 const HEADERS = {
-  headers: new Headers({
+  headers: {
     Authorization: `Bearer ${TOKEN_API}`
-  })
+  }
 };
 
 export {
